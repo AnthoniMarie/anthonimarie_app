19814b6602cd3ef1117c389aa5aaf95f3670dde2
Anthoni Marie <contact@anthoni-marie.fr>
2021-02-05 13:47:17 +0100

    [+] - Starting to develop the possibily for users to get a notification when i post a new blog on my website...

--- App.tsx
+++ App.tsx
@@ -5,6 +5,19 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+const registerForPushNotifications = async () => { 
+  try {
+     const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+     if (!permission.granted) return;
+     const token = await Notifications.getExpoPushTokenAsync();
+  console.log(token);
+  } catch (error) {
+    console.log('An error has occured while trying to get our beautiful push token...', error);
+  }
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,6 +26,7 @@ export default function App() {
   if (!isLoadingComplete) {
     return null;
   } else {
+    registerForPushNotifications();
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
