c168d60cff5ad0fba55d03e662ed41257b469e05
Anthoni Marie <contact@anthoni-marie.fr>
2021-02-05 22:18:58 +0100

    [TEST] - Testing purposes to handle firebase connections. The goal is to save in the database expo tokens for my notifications...

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import Config from 'react-native-config'
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+//Notifications
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
+// Firebase linkage imports
+import firebase from 'firebase/app';
+import "firebase/database";
+import "firebase/auth";
+
+// Firebase Initialization
+const firebaseConfig = {
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  databaseURL: Config.DATABASE_URL,
+  projectId: Config.PROJECT_ID,
+  storageBucket: Config.STORAGE_BUCKET,
+  messagingSenderId: Config.MESSAGING_SENDER_ID,
+};
+firebase.initializeApp(firebaseConfig);
 
 const registerForPushNotifications = async () => { 
   try {
-     const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-     if (!permission.granted) return;
-     const token = await Notifications.getExpoPushTokenAsync();
-  console.log(token);
-  } catch (error) {
+    const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (!permission.granted) return;
+    const token = await Notifications.getExpoPushTokenAsync();
+    var updates = {}
+    var key = firebase.database().ref('/contacts').push().key
+    //firebase.auth().signInAnonymously();
+
+    firebase.database().ref('/contacts').child(key).set({ push_token: token })
+    console.log(token);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        var uid = user.uid;
+        console.log(user.uid);
+      }
+    });
+    } catch (error) {
     console.log('An error has occured while trying to get our beautiful push token...', error);
   }
 }
