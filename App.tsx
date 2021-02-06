4499e0c8159fc214f756da37aff371836138423b
Anthoni Marie <contact@anthoni-marie.fr>
2021-02-06 13:47:31 +0100

    [TEST] - Testing purposes with Firebase

--- App.tsx
+++ App.tsx
@@ -1,46 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Config from 'react-native-config'
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-//Notifications
+
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
-// Firebase linkage imports
+
 import firebase from 'firebase/app';
 import "firebase/database";
 import "firebase/auth";
 
 // Firebase Initialization
 const firebaseConfig = {
-  apiKey: Config.FIREBASE_API_KEY,
-  authDomain: Config.AUTH_DOMAIN,
-  databaseURL: Config.DATABASE_URL,
-  projectId: Config.PROJECT_ID,
-  storageBucket: Config.STORAGE_BUCKET,
-  messagingSenderId: Config.MESSAGING_SENDER_ID,
+  apiKey: 'AIzaSyBPVau-TMjJnLFMtb6liuGSAdSpZN3rFL8',
+  authDomain: 'anthonimarieapp-af414.firebaseapp.com',
+  databaseURL: 'https://anthonimarieapp-af414-default-rtdb.firebaseio.com/',
+  projectId: 'anthonimarieapp-af414',
+  storageBucket: 'anthonimarieapp-af414.appspot.com',
+  messagingSenderId: '654775663112',
 };
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length)
+  firebase.initializeApp(firebaseConfig);
+else
+  firebase.app();
 
 const registerForPushNotifications = async () => { 
   try {
     const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
     if (!permission.granted) return;
     const token = await Notifications.getExpoPushTokenAsync();
-    var updates = {}
-    var key = firebase.database().ref('/contacts').push().key
-    //firebase.auth().signInAnonymously();
 
-    firebase.database().ref('/contacts').child(key).set({ push_token: token })
-    console.log(token);
+    firebase.auth().signInAnonymously();
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         var uid = user.uid;
+        firebase.database().ref('users/' + user.uid).set({notif_token: token,});
         console.log(user.uid);
       }
     });
+    console.log(token);
     } catch (error) {
     console.log('An error has occured while trying to get our beautiful push token...', error);
   }
